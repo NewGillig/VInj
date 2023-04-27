374db115c8d7050cf62ad0b7695895dd2d27fb4e
NewGillig <sixhaongmao@hotmail.com>
2023-04-27 13:53:55 -0700

    Delete loc_train_test.sh

--- loc_train_test.sh
+++
@@ -1,9 +0,0 @@
-cd reuse_dataset
-python proc_loc.py
-./tokenize loc_train
-./tokenize loc_test
-cp -r loc_train ../localization/M1_VulRepair_PL-NL/
-cp -r loc_test ../localization/M1_VulRepair_PL-NL/
-cd ../localization/M1_VulRepair_PL-NL/
-bash train.sh
-bash test.sh
