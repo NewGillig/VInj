af43a45e3e177760ed8b45704ca284e4f5629540
NewGillig <sixhaongmao@hotmail.com>
2023-04-27 13:54:08 -0700

    Delete pattern_train_test.sh

--- pattern_train_test.sh
+++
@@ -1,8 +0,0 @@
-cd pattern_mining_applying
-python parse.py reuse_train 0
-python parse.py reuse_test 0
-python get_dataset_pkl.py
-pypy3 testHierarchical.py
-bash cluster.sh
-python copy_loc.py
-pypy3 testApplying.py
